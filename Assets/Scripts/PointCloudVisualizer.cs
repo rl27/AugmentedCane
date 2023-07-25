f2afa8e5b6f366983aab6f3f2df9f2ec2ff09b30
rl27 <rl27@princeton.edu>
2023-07-24 21:23:39 -0400

    Add optional grid system for point clouds

--- Assets/Scripts/PointCloudVisualizer.cs
+++ Assets/Scripts/PointCloudVisualizer.cs
@@ -14,6 +14,11 @@ public class PointCloudVisualizer : MonoBehaviour
 
     // Keep track of all points.
     public Dictionary<ulong, Vector3> points = new Dictionary<ulong, Vector3>();
+
+    // Group points based on location.
+    public Dictionary<Vector3, Dictionary<ulong, Vector3>> grid = new Dictionary<Vector3, Dictionary<ulong, Vector3>>();
+    float gridScale = 10f;
+
     public HashSet<Vector3> points2 = new HashSet<Vector3>();
     public Dictionary<ulong, float> confidences = new Dictionary<ulong, float>();
 
@@ -33,6 +38,8 @@ public class PointCloudVisualizer : MonoBehaviour
 
     // Whether to use built-in point cloud mechanisms or manually create points from depth.
     private bool useDepth = false;
+
+    private bool useGrid = true;
 
     void Awake()
     {
@@ -77,7 +84,7 @@ public class PointCloudVisualizer : MonoBehaviour
         }
 
         // Remove any extra pre-existing particles
-        for (int i = index; i < prevNumParticles; i++)
+        for (int i = numParticles; i < prevNumParticles; i++)
             particles[i].remainingLifetime = -1f;
 
         particleSystem.SetParticles(particles, Math.Max(numParticles, prevNumParticles));
@@ -113,6 +120,39 @@ public class PointCloudVisualizer : MonoBehaviour
         prevNumParticles = numParticles;
     }
 
+    void UpdateParticles3()
+    {
+        // Create or resize particle array if necessary
+        int numParticles = points.Count; // Set this to positions.Length if only rendering points in the current frame.
+        if (particles == null || particles.Length < numParticles)
+            particles = new ParticleSystem.Particle[(int) (1.5 * numParticles)]; // Create an array with extra space to reduce re-creations.
+
+        Color32 startColor = particleSystem.main.startColor.color;
+        float startSize = particleSystem.main.startSize.constant;
+        int index = 0;
+        foreach (var gtp in grid) {
+            var d = gtp.Value;
+            foreach (var kvp in d) {
+                Vector3 pos = kvp.Value;
+                if (pos.y < planeSource.min + heightToDiscard)
+                    particles[index].startColor = Color.red;
+                else
+                    particles[index].startColor = startColor;
+                particles[index].startSize = startSize;
+                particles[index].position = pos;
+                particles[index].remainingLifetime = 1f;
+                index++;
+            }
+        }
+
+        // Remove any extra pre-existing particles
+        for (int i = numParticles; i < prevNumParticles; i++)
+            particles[i].remainingLifetime = -1f;
+
+        particleSystem.SetParticles(particles, Math.Max(numParticles, prevNumParticles));
+        prevNumParticles = numParticles;
+    }
+
     void OnPointCloudUpdated(ARPointCloudUpdatedEventArgs eventArgs)
     {
         /** UPDATE POINTS **/
@@ -126,11 +166,9 @@ public class PointCloudVisualizer : MonoBehaviour
 
                     if (depthSource.GetConfidence(x, y) != 255)
                         continue;
-                    
-                    float scale = 25f;
+
                     Vector3 v = depthSource.TransformLocalToWorld(depthSource.ComputeVertex(x, y, depthSource.GetDepth(x, y)));
-                    v = new Vector3(Mathf.Round(v.x * scale) / scale, Mathf.Round(v.y * scale) / scale, Mathf.Round(v.z * scale) / scale);
-                    points2.Add(v);
+                    points2.Add(SnapToGrid(v));
                 }
             }
 
@@ -144,10 +182,6 @@ public class PointCloudVisualizer : MonoBehaviour
             var positions = pointCloud.positions.Value;
             var identifiers = pointCloud.identifiers.Value;
 
-            // Create/update positions in dictionary
-            for (int i = 0; i < positions.Length; i++)
-                points[identifiers[i]] = positions[i];
-
             // ARKit does not provide point cloud confidence values. https://forum.unity.com/threads/getconfidence-method-for-arkit-pointcloud.614920
             if (pointCloud.confidenceValues.HasValue) {
                 var conf = pointCloud.confidenceValues.Value;
@@ -155,7 +189,39 @@ public class PointCloudVisualizer : MonoBehaviour
                     confidences[identifiers[i]] = conf[i];
             }
 
-            UpdateParticles();
+            if (!useGrid) {
+                // Create/update positions in dictionary
+                for (int i = 0; i < positions.Length; i++)
+                    points[identifiers[i]] = positions[i];
+
+                UpdateParticles();
+            }
+            else {
+                // Create/update positions in dictionary
+                for (int i = 0; i < positions.Length; i++) {
+                    ulong id = identifiers[i];
+                    Vector3 pos = positions[i];
+                    Vector3 gridPoint = SnapToGrid(pos);
+
+                    // If this specific point already exists, remove it from the grid dict and update update the listing.
+                    if (points.ContainsKey(id))
+                        grid[points[id]].Remove(id);
+                    points[id] = gridPoint;
+
+                    // Add to (or update) grid dict
+                    if (!grid.ContainsKey(gridPoint))
+                        grid[gridPoint] = new Dictionary<ulong, Vector3>();
+                    else
+                        grid[gridPoint][id] = pos;
+                }
+
+                UpdateParticles3();
+            }
         }
+    }
+
+    Vector3 SnapToGrid(Vector3 v)
+    {
+        return new Vector3(Mathf.Round(v.x * gridScale) / gridScale, Mathf.Round(v.y * gridScale) / gridScale, Mathf.Round(v.z * gridScale) / gridScale);
     }
 }
