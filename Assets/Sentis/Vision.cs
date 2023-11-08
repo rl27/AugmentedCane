ef0ea1b30ba1041d92afa91b0ee1abcdc7f90149
rl27 <raymond.liu1234@gmail.com>
2023-11-07 22:37:06 -0500

    Revert to previous model

--- Assets/Sentis/Vision.cs
+++ Assets/Sentis/Vision.cs
@@ -54,7 +54,7 @@ public class Vision : MonoBehaviour
 
     private Unity.Sentis.BackendType backendType;
 
-    public static readonly Color32[] COLOR_TABLE_10 = new Color32[]
+    public static readonly Color32[] COLOR_TABLE = new Color32[]
     {
         new Color32(0, 0, 0, 255), // background
         new Color32(0, 0, 255, 255), // road
@@ -68,47 +68,13 @@ public class Vision : MonoBehaviour
         new Color32(0, 0, 255, 255), // rail track
     };
 
-    /*
-    public static readonly Color32[] COLOR_TABLE = new Color32[]
-    {
-        new Color32(0, 0, 255, 255), // road
-        new Color32(255, 0, 255, 255), // crosswalk
-        new Color32(0, 255, 0, 255), // sidewalk
-        new Color32(255, 0, 0, 255), // covering
-        new Color32(150, 150, 150, 255), // barrier
-        new Color32(70, 70, 70, 255), // structure
-        new Color32(153, 0, 204, 255), // person
-        new Color32(255, 102, 0, 255), // vehicle
-        new Color32(128, 96, 0, 255), // objects
-        new Color32(0, 150, 0, 255), // terrain
-        new Color32(0, 70, 0, 255), // tree
-        new Color32(0, 0, 150, 255), // water
-        new Color32(0, 255, 255, 255) // sky
-    };*/
-    public static readonly Color32[] COLOR_TABLE = new Color32[]
-    {
-        new Color32(0, 0, 255, 255), // road
-        new Color32(255, 0, 255, 255), // crosswalk
-        new Color32(0, 255, 0, 255), // sidewalk
-        new Color32(255, 0, 0, 255), // covering
-        new Color32(0, 0, 0, 255), // barrier
-        new Color32(0, 0, 0, 255), // structure
-        new Color32(0, 0, 0, 255), // person
-        new Color32(0, 0, 0, 255), // vehicle
-        new Color32(0, 0, 0, 255), // objects
-        new Color32(0, 150, 0, 255), // terrain
-        new Color32(0, 0, 0, 255), // tree
-        new Color32(0, 0, 0, 255), // water
-        new Color32(0, 0, 0, 255) // sky
-    };
-
     // Sidewalk or crosswalk
     private bool StrictWalkable(int cls) {
-        return (cls == 1 || cls == 2);
+        return (cls >= 4 && cls <= 6);
     }
     // Sidewalk, crosswalk, curb, covering
     private bool LaxWalkable(int cls) {
-        return (cls >= 1 && cls <= 3);
+        return (cls >= 2 && cls <= 8);
     }
 
     void Start()
@@ -407,27 +373,30 @@ public class Vision : MonoBehaviour
         if (lastClass != cls && (DateTime.Now - lastClassChange).TotalSeconds > classChangeInterval) {
             switch (cls)
             {
-                case 0: // Road
+                case 1: // Road
                     lastClass = cls;
                     lastClassChange = DateTime.Now;
                     tts.EnqueueTTS(road);
                     logging = "Road";
                     break;
-                case 2: // Sidewalk
+                case 4: // Sidewalk
                     lastClass = cls;
                     lastClassChange = DateTime.Now;
                     tts.EnqueueTTS(sidewalk);
                     logging = "Sidewalk";
                     break;
-                case 1: // Crosswalk
+                case 5: // Crosswalk
+                case 6:
                     lastClass = cls;
                     lastClassChange = DateTime.Now;
                     tts.EnqueueTTS(crosswalk);
                     logging = "Crosswalk";
                     break;
-                default:
+                case 0: // Background
                     lastClass = cls;
                     logging = "Unknown";
+                    break;
+                default:
                     break;
             }
         }
